d4449a26673e9274ff7b11438a9fec0ac56f4c0d
Ken Lu <ken@seeice.com>
2012-01-18 16:39:53 -0500

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CaptchaDemo::Application.config.secret_token = '5bc7414d0b4cc2eb00e2c47d0749f6cb26c6ce8937007dc27457c7c863a4263711947b8d97ba58f24aa75be812e533019b57026166dbd2ff2fa891567e8960c5'
